976fdff5ff36d16778f1b10735fd97a633145753
washington Luiz Silva de Oliveira <wasluizoliveira@gmail.com>
2025-08-11 10:10:48 -0300

    Eu fiz mas está com problema de dotnetframework 3.5.

---
+++ SuperSoldier/Source/MySuperStrengthMod/CompSuperStrength.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace MySuperStrengthMod
+{
+    public class CompProperties_SuperStrength : CompProperties
+    {
+        public int strengthLevel = 1;
+        public CompProperties_SuperStrength() => this.compClass = typeof(CompSuperStrength);
+    }
+
+    public class CompSuperStrength : ThingComp
+    {
+        public CompProperties_SuperStrength Props => (CompProperties_SuperStrength)this.props;
+        public int StrengthLevel => Props.strengthLevel;
+    }
+
+}
---
+++ SuperSoldier/Source/MySuperStrengthMod/SuperStrengthPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using MySuperStrengthMod;
+using RimWorld;
+using Verse;
+
+[StaticConstructorOnStartup]
+public static class SuperStrengthPatch
+{
+    static SuperStrengthPatch()
+    {
+        var harmony = new Harmony("SuperSoldier.RimWorld");
+        //harmony.Patch(AccessTools.Property(typeof(Pawn), "CarryingCapacity").GetGetMethod(),
+        harmony.Patch(AccessTools.PropertyGetter(typeof(PawnCapacityDef), "CarryingCapacity"),
+        postfix: new HarmonyMethod(typeof(SuperStrengthPatch), nameof(Postfix_CarryingCapacity)));
+    }
+
+    public static void Postfix_CarryingCapacity(Pawn __instance, StatDef stat, bool applyPostProcess, ref float __result)
+    {
+        if (stat == StatDefOf.CarryingCapacity)
+        {
+            var comp = __instance.TryGetComp<CompSuperStrength>();
+            if (comp != null && comp.StrengthLevel > 0)
+            {
+                __result += comp.StrengthLevel * 10f; // ex.: +10 kg por nível
+            }
+        }
+    }
+}
